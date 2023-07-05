9be0c86239791227a9e6abd03461e298490bb8e5
Anton <artiuhovanton21@gmail.com>
2023-07-05 22:02:23 +0300

    Code: Расчет количества чисел в столбцах вынесен в отдельную функцию

--- Practice_ex3/Practice_ex3.cpp
+++ Practice_ex3/Practice_ex3.cpp
@@ -23,36 +23,36 @@ void find_minmax(const vector<double> &numbers, double& min, double& max) {
     }
     
 }
+vector<size_t> make_histogram(size_t &bin_count,const vector<double>& numbers, double& min, double& max) {
+    vector<size_t> bins(bin_count);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count) {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
+}
 
 int main() {
     char decision;
     do {
         // Ввод данных
-        size_t number_count;
+        const size_t SCREEN_WIDTH = 80;
+        const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+        size_t number_count, bin_count;
+        double min, max;
         cerr << "Enter number count: ";
         cin >> number_count;
         cerr << "Enter numbers: ";
         const auto numbers = input_numbers(number_count);
-        size_t bin_count;
         cerr << "Enter column count: ";
         cin >> bin_count;
-
         // Обработка данных
-        double min, max;
         find_minmax(numbers, min, max);
-        vector<size_t> bins(bin_count);
-        for (double number : numbers) {
-            size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-            if (bin == bin_count) {
-                bin--;
-            }
-            bins[bin]++;
-        }
-
+        const auto bins = make_histogram(bin_count, numbers, min, max);
         // Вывод данных
-        const size_t SCREEN_WIDTH = 80;
-        const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-
         size_t max_count = 0;
         for (size_t count : bins) {
             if (count > max_count) {
