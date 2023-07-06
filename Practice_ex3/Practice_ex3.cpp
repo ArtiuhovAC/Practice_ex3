758a842f5cb5e6616c267cb4c8810414ed84da2e
Anton <artiuhovanton21@gmail.com>
2023-07-06 17:42:39 +0300

    Code: Вывод программы переделан для SVG файлов

--- Practice_ex3/Practice_ex3.cpp
+++ Practice_ex3/Practice_ex3.cpp
@@ -1,9 +1,11 @@
 ﻿#include <iostream>
 #include <vector>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <sstream>
 using namespace std;
-// сделать контроль ширины
-const size_t SCREEN_WIDTH = 80;
-const size_t IMAGE_HEIGHT = 20;
 vector <double> input_numbers(size_t count) {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
@@ -22,7 +24,6 @@ void find_minmax(const vector<double> &numbers, double& min, double& max) {
             max = number;
         }
     }
-    
 }
 vector<size_t> make_histogram(size_t &number_count,size_t &bin_count,const vector<double>& numbers, double& min, double& max) {
     vector<size_t> bins(bin_count);
@@ -40,58 +41,86 @@ vector<size_t> make_histogram(size_t &number_count,size_t &bin_count,const vecto
         return bins;
     }
 }
-void show_histogram_text(vector<size_t> &bins) {
+
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void svg_end() {
+    cout << "</svg>\n";
+}
+void svg_rect(double x, double y, double width, double height, string stroke, string fill) {
+    cout << "<rect x='" << x << "' y ='" << y << "' width='" << width <<"' height='" << height << "' stroke=' "<< stroke << "' fill='"<<fill
+        << "'/>" << '\n';
+}
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='"<< left << "' y='"<<baseline<<"'>"<< text <<" </text>"<<'\n';
+
+}
+void show_histogram_svg(vector<size_t>& bins, const size_t& bin_count) {
+    const auto IMAGE_WIDTH = 810;
+    const auto IMAGE_HEIGHT = 745;
+    const auto IMAGE_HEIGHT2 = 730;
+    const auto TEXT_SDVIG = 15;
+    const auto TEXT_WIDTH = 50;
+    const auto MASHTAB = 10;
+    const auto RANDOM_COLOR = 3;
+    const size_t MAX_ASTERISK = 73;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     size_t max_count = 0;
+    size_t height;
+    size_t x = 0, y;
     for (size_t count : bins) {
         if (count > max_count) {
             max_count = count;
         }
     }
-    const bool scaling_needed = max_count > IMAGE_HEIGHT;
-    for (size_t i = 0; i < bins.size();i++)
-    if (scaling_needed) {
-        const double scaling_factor = (double)IMAGE_HEIGHT / max_count;
-        if (bins[i] == max_count)
-            bins[i] = IMAGE_HEIGHT;
-        else
-            bins[i] = (size_t)(bins[i] * scaling_factor);
-    }
-    size_t height;
-    if (scaling_needed) {
-        height = IMAGE_HEIGHT;
-    }
-    else height = max_count;
-    for (size_t i = height; i > 0; i--) {
-        for (size_t j = 0; j < bins.size(); j++) {
-            if (bins[j] > 9) {
-                cout << " ";
-            }
-            if (bins[j] >= i) {
-                cout << "*";
+    const bool scaling_needed = max_count  > MAX_ASTERISK;
+    for (size_t i = 0; i < bins.size(); i++)
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            if (bins[i] != max_count)
+                bins[i] = (size_t)(bins[i] * scaling_factor);
+        }
+    for (size_t i = 0; i < bins.size(); i++)
+    {
+        if (bins[i] != max_count)
+            bins[i] *= MASHTAB;
+        else {
+            if (scaling_needed) {
+                bins[i] = IMAGE_HEIGHT2;
+                height = IMAGE_HEIGHT2;
             }
             else {
-                cout << " ";
+                bins[i] *= MASHTAB;
+                height = bins[i];
+                
             }
-            cout << " ";
         }
-        cout << '\n';
     }
-    for (size_t i = 0; i < bins.size(); i++) {
-        if (bins[i] > 9)
-            cout << "__";
-        else
-            cout << "_";
-        cout << " ";
+    for (int i = 0; i < bin_count; i++) {
+        string stroke = "#", fill= "#";
+        for (int j = 0; j < RANDOM_COLOR; j++) {
+            stringstream stream;
+            stream << hex << rand() % 256;
+            string result(stream.str());
+            stroke += result;
+            fill += result;
+        }
+        y = height - bins[i];
+        svg_rect(x, y, TEXT_WIDTH, bins[i], stroke, fill);
+        svg_text(x +TEXT_SDVIG , height + TEXT_SDVIG, to_string(bins[i] / MASHTAB));
+        x += TEXT_WIDTH;
     }
-    cout << '\n';
-    for (size_t i = 0; i < bins.size(); i++)
-        cout << bins[i]<<" ";
-    cout << '\n';
+    svg_end();
 }
+
 int main() {
-    
-    char decision;
-    do {
+        srand(time(NULL));
         size_t number_count, bin_count;
         double min, max;
         cerr << "Enter number count: ";
@@ -101,12 +130,6 @@ int main() {
         cerr << "Enter column count: ";
         cin >> bin_count;
         find_minmax(numbers, min, max);
-        auto bins = make_histogram(number_count,bin_count, numbers, min, max);
-        show_histogram_text(bins);
-        cerr << "Enter '1' if you arent satisfied, else enter other symbols: ";
-        cin >> decision;
-        if (decision == '1')
-            cout << "\n";
-
-    } while (decision == '1');
+        auto bins = make_histogram(number_count,bin_count, numbers, min, max);  
+        show_histogram_svg(bins,bin_count);
 }
