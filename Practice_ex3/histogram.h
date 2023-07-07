c3980e2dfbf90a11492c39ed2ff87c87e63cc4aa
Anton <artiuhovanton21@gmail.com>
2023-07-07 11:39:04 +0300

    code: дополнен модуль histogram

--- Practice_ex3/histogram.h
+++ Practice_ex3/histogram.h
@@ -9,3 +9,9 @@
 using namespace std;
 void find_minmax(const vector<double>& numbers, double& min, double& max);
 vector<size_t> make_histogram(const size_t& number_count,const size_t& bin_count, const vector<double>& numbers,const double& min,const double& max);
+vector <double> input_numbers(size_t count);
+void svg_begin(double width, double height);
+void svg_end();
+void svg_rect(double x, double y, double width, double height, string stroke, string fill);
+void svg_text(double left, double baseline, string text);
+void show_histogram_svg(vector<size_t>& bins, const size_t& bin_count);
