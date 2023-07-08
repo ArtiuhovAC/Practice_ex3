dd7c7bb40cb7e34f3ef335b6c69ebfd21cd98b49
Anton <artiuhovanton21@gmail.com>
2023-07-08 12:51:14 +0300

    Code : Добавлена обработка ошибок

--- Practice_ex3/Practice_ex3.cpp
+++ Practice_ex3/Practice_ex3.cpp
@@ -13,18 +13,22 @@
 using namespace std;
 int main(int argc, char* argv[]) {
         srand(time(NULL));
-        curl_global_init(CURL_GLOBAL_ALL);
+        
         if (argc > 1) {
             CURL* curl = curl_easy_init();
             if (curl) {
                 CURLcode res;
                 curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
                 res = curl_easy_perform(curl);
-                curl_easy_cleanup(curl);
+                if (res != 0) {
+                    cout << curl_easy_strerror(res);
+                    exit(1);
+                }
             }
             curl_easy_cleanup(curl);
             return 0;
         }
+        curl_global_init(CURL_GLOBAL_ALL);
         Input data;
         data = read_input(cin,true);
         auto bins = make_histogram(data);  
