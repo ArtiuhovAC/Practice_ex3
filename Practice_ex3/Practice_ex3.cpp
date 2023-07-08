da97a4b6806a169ff1ac00224499d45101c94fa9
Anton <artiuhovanton21@gmail.com>
2023-07-08 11:04:38 +0300

    Code: Добавлено получение аргументов в main

--- Practice_ex3/Practice_ex3.cpp
+++ Practice_ex3/Practice_ex3.cpp
@@ -11,9 +11,15 @@
 #include "curl/curl.h"
 
 using namespace std;
-int main() {
+int main(int argc, char* argv[]) {
         srand(time(NULL));
         curl_global_init(CURL_GLOBAL_ALL);
+        if (argc > 1) {
+            for (int i = 1; i < argc; i++) {
+                cout << argv[i] << " ";
+            }
+            return 0;
+        }
         Input data;
         data = read_input(cin,true);
         auto bins = make_histogram(data);  
