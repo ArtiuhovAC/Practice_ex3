a3bcc93a88fcee0f7edfe70fd87fbcec3f639af9
Anton <artiuhovanton21@gmail.com>
2023-07-08 11:18:46 +0300

    Code: Добавлена работа с URL

--- Practice_ex3/Practice_ex3.cpp
+++ Practice_ex3/Practice_ex3.cpp
@@ -15,9 +15,14 @@ int main(int argc, char* argv[]) {
         srand(time(NULL));
         curl_global_init(CURL_GLOBAL_ALL);
         if (argc > 1) {
-            for (int i = 1; i < argc; i++) {
-                cout << argv[i] << " ";
+            CURL* curl = curl_easy_init();
+            if (curl) {
+                CURLcode res;
+                curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+                res = curl_easy_perform(curl);
+                curl_easy_cleanup(curl);
             }
+            curl_easy_cleanup(curl);
             return 0;
         }
         Input data;
